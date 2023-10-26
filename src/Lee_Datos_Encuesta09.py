25efb6d2e0a57d840f2492e46aa3303de5fb8ade
edveralli <edveralli@gmail.com>
2023-10-26 12:43:53 -0300

    fixed bug in Drupal order & catched divided by zero in No Aplicas

--- src/Lee_Datos_Encuesta09.py
+++ src/Lee_Datos_Encuesta09.py
@@ -79,10 +79,9 @@ df.insert(1, "puntuacion",0)
 pepe = (df.columns)
 
 
+#df = df.reindex(["Creado","puntuacion","Apellido y Nombre","¿Pertenecés a un área del Gobierno de la Ciudad de Buenos Aires?"," Mail","Cargo/puesto que ocupa","¿De que secretaría o subsecretaría depende?","1- ¿Recopilan datos?","2- ¿Tiene Base de datos propias?","3- ¿Quién controla y gestiona la base de datos?","4- ¿Integran los datos de las distintas fuentes que disponen?","5- ¿Utilizan alguna tecnología para integrar  los datos, de diferentes fuentes y sistemas? (APIs, Middleware, Data Warehouse)","6- ¿Tienen documentadas las políticas de integración de datos?","7- ¿Tiene un área destinada a Ciencia de datos?","8- ¿Aplican técnicas estadísticas y modelos matemáticos para analizar los datos?","9- ¿Utilizan herramientas de visualización para crear gráficos y tablas que permitan mostrar el  resultado del análisis de los datos?","10.- ¿Se desarrollan y entrenan modelos de aprendizaje automático y análisis predictivo para predecir patrones y eventos futuros?","11.- ¿Tienen documentados los proyectos de ciencia de datos?","12.- ¿Tienen reportes?","13- ¿Con qué frecuencia actualizan los reportes?","14.- ¿Cuál es la forma de actualización de los reportes?","15.-¿ Los datos están disponibles para las áreas que los requieran?","16.- ¿Los datos se pueden leer usando alguno de los formatos estándares abiertos?","17.- ¿Tienen APIs desarrolladas para que usuarios externos puedan acceder a los datos?","18.- ¿Conoce la normativa de protección de Datos Personales?","19. ¿Está definido por cuánto tiempo se deben guardar los datos personales en el sistema?","20. ¿Tienen clasificados los datos sensibles?","21- ¿Los datos sensibles son recogidos con consentimiento?","22- ¿Existe un mecanismo de minimización o destrucción periódica de los datos?","23- ¿Se generan espacios de diálogos /comités para hablar sobre diferentes temas o evacuar dudas sobre políticas de datos?","24- ¿Tienen lineamientos de protección de datos definidos?","25- ¿Tienen relevados roles respecto a los datos?","26- ¿Existen políticas sobre quién puede utilizar los datos, cómo pueden usarlos, qué partes pueden usar y con qué propósitos?","27- ¿Tienen documentadas las etapas del ciclo de vida de los datos?","28- ¿Realizan capacitaciones para el personal en gobernanza de datos (clasificación, roles)?","29.- ¿Tienen reportes de acceso a los datos?","30.-¿Tienen implementado algún sistema de acceso a la información?","31-   ¿Realizan controles de revisión de la calidad de los datos con cierta periodicidad?","32-  ¿Qué métricas de calidad analizan esos controles?","33- ¿Tienen procesos y procedimientos documentados que analicen el grado de cumplimiento de las métricas de calidad definidas?","34- ¿Tienen perfiles profesionales/técnicos dedicados a evaluar con una periodicidad conocida métricas de calidad?","35- ¿Se intercambian datos...?","36- ¿Tienen definidos mecanismos para que las personas de la organización compartan los datos con un propósito determinado?","37- ¿Tienen perfiles profesionales/técnicos especializados en el tema?","38- ¿Tienen definido un modelo de datos?","39 ¿Qué tan bien documentados están los datos?","40 ¿Tienen documentación del modelo de datos que defina...","41- ¿Tienen personas trabajando que se especialicen en el tema?","Sugerencias"], axis=1)
 
-df = df.reindex(["Creado","puntuacion","Apellido y Nombre","¿Pertenecés a un área del Gobierno de la Ciudad de Buenos Aires?"," Mail","Cargo/puesto que ocupa","¿De que secretaría o subsecretaría depende?","1- ¿Recopilan datos?","2- ¿Tiene Base de datos propias?","3- ¿Quién controla y gestiona la base de datos?","4- ¿Integran los datos de las distintas fuentes que disponen?","5- ¿Utilizan alguna tecnología para integrar  los datos, de diferentes fuentes y sistemas? (APIs, Middleware, Data Warehouse)","6- ¿Tienen documentadas las políticas de integración de datos?","7- ¿Tiene un área destinada a Ciencia de datos?","8- ¿Aplican técnicas estadísticas y modelos matemáticos para analizar los datos?","9- ¿Utilizan herramientas de visualización para crear gráficos y tablas que permitan mostrar el  resultado del análisis de los datos?","10.- ¿Se desarrollan y entrenan modelos de aprendizaje automático y análisis predictivo para predecir patrones y eventos futuros?","11.- ¿Tienen documentados los proyectos de ciencia de datos?","12.- ¿Tienen reportes?","13- ¿Con qué frecuencia actualizan los reportes?","14.- ¿Cuál es la forma de actualización de los reportes?","15.-¿ Los datos están disponibles para las áreas que los requieran?","16.- ¿Los datos se pueden leer usando alguno de los formatos estándares abiertos?","17.- ¿Tienen APIs desarrolladas para que usuarios externos puedan acceder a los datos?","18.- ¿Conoce la normativa de protección de Datos Personales?","19. ¿Está definido por cuánto tiempo se deben guardar los datos personales en el sistema?","20. ¿Tienen clasificados los datos sensibles?","21- ¿Los datos sensibles son recogidos con consentimiento?","22- ¿Existe un mecanismo de minimización o destrucción periódica de los datos?","23- ¿Se generan espacios de diálogos /comités para hablar sobre diferentes temas o evacuar dudas sobre políticas de datos?","24- ¿Tienen lineamientos de protección de datos definidos?","25- ¿Tienen relevados roles respecto a los datos?","26- ¿Existen políticas sobre quién puede utilizar los datos, cómo pueden usarlos, qué partes pueden usar y con qué propósitos?","27- ¿Tienen documentadas las etapas del ciclo de vida de los datos?","28- ¿Realizan capacitaciones para el personal en gobernanza de datos (clasificación, roles)?","29.- ¿Tienen reportes de acceso a los datos?","30.-¿Tienen implementado algún sistema de acceso a la información?","31-   ¿Realizan controles de revisión de la calidad de los datos con cierta periodicidad?","32-  ¿Qué métricas de calidad analizan esos controles?","33- ¿Tienen procesos y procedimientos documentados que analicen el grado de cumplimiento de las métricas de calidad definidas?","34- ¿Tienen perfiles profesionales/técnicos dedicados a evaluar con una periodicidad conocida métricas de calidad?","35- ¿Se intercambian datos...?","36- ¿Tienen definidos mecanismos para que las personas de la organización compartan los datos con un propósito determinado?","37- ¿Tienen perfiles profesionales/técnicos especializados en el tema?","38- ¿Tienen definido un modelo de datos?","39 ¿Qué tan bien documentados están los datos?","40 ¿Tienen documentación del modelo de datos que defina...","41- ¿Tienen personas trabajando que se especialicen en el tema?","Sugerencias"], axis=1)
-
-
+df = df.iloc[:, [0,1,2,5,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48]]
 
 df.to_csv('respuesta_final.csv', index=False, encoding='utf-8',sep=',')
 
@@ -90,13 +89,13 @@ df2 = pd.read_csv("respuesta_final.csv", names=colnames,index_col=False, encodin
 
 df2 = df2.drop(labels=0, axis=0)
 df2 = df2.fillna('None')  ## cambio los NaN por 0's
-df2.to_csv('Responses.csv', index=False, encoding='utf-8',sep=',')
 
+df2.to_csv('Responses.csv', index=False, encoding='utf-8',sep=',')
 
 # Aislo el organismo emisor de las respuestas
 organismos  = df2[['fecha','score','nombre_apellido','pertenece_gcba' ,'mail','cargo','secre_subse_area']]
 ## TENGO QUE REEMPLAZAR LAS RESPUESTAS CON LETRAS POR NUMEROS
-codigos = pd.read_csv('libro_codigos_16_06.csv',sep=',', encoding='utf-8')
+codigos = pd.read_csv('libro_codigos_31_07.csv',sep=',', encoding='utf-8')
 
 codigos2 = {}
 
@@ -160,18 +159,21 @@ f = open("nuevo_df.csv", "w")
 linea = 'fecha'+";"+'score'+";"+'nombre_apellido'+";"+'pertenece_gcba'+";"+'mail'+";"+"cargo"+";"+'secre_subse_area'+" ; "+'p1_recop_dat'+" ; "+'p2_bases_dat'+" ; "+'p3_control_base'+" ; "+'p4_integr_dat'+" ; "+'p5_tec_integr_dat'+" ; "+'p6_documen_pol_int'+" ; "+'p7_area_cienc_dat'+" ; "+'p8_apli_tec_ciencia_dat'+" ; "+'p9_herra_vis_dat'+" ; "+'p10_apli_pred'+" ; "+'p11_documen_proy_cie_dat'+" ; "+'p12_tien_report'+" ; "+'p13_act_report'+" ; "+'p14_forma_act_report'+" ; "+'p15_disp_dat'+" ; "+'p16_lect_dat_stand'+" ; "+'p17_api_dat'+" ; "+'p18_norm_prot_dat'+" ; "+'p19_tiemp_dat'+" ; "+'p20_clasif_dat_sens'+" ; "+'p21_dat_sens_consent'+" ; "+'p22_destr_minim_dat'+" ; "+'p23_espac_disc_prot_dat'+" ; "+'p24_lineam_pro_dat'+" ; "+'p25_roles_dat'+" ; "+'p26_polit_uso_dat'+" ; "+'p27_documen_cic_v_dat'+" ; "+'p28_capac_pers_gobern_dat'+" ; "+'p29_report_acc_dat'+" ; "+'p30_sist_acc_info'+" ; "+'p31_revis_cal_dat'+" ; "+'p32_metr_cal_dat'+" ; "+'p33_proc_cal_dat'+" ; "+'p34_per_espec_cal_dat'+" ; "+'p35_interc_dat'+" ; "+'p36_mecan_prop_comp_dat'+" ; "+'p37_pers_espec_reut_dat'+" ; "+'p38_model_dat'+" ; "+'p39_grad_documen_dat'+" ; "+'p40_documen_mod_dat'+" ; "+'p41_pers_espec_model_dat'+" ; "+'sugerencias'
 f.write(linea+"\n")
 
+
 for i in df2.index: 
     # print(df2['p1_recop_dat'][i])
     # print(codigos2.get('p1_recop_dat'''+"-"+df2['p1_recop_dat'][i]))
     # print(str(codigos2.get('p1_recop_dat'''+"-"+df2['p1_recop_dat'][i])))
     
     # print("**********")
-    linea = df2['fecha'][i]+" ; "+df2['score'][i]+";"+df2['nombre_apellido'][i]+";"+df2['pertenece_gcba'][i]+";"+df2['mail'][i]+";"+df2['cargo'][i]+";"+df2['secre_subse_area'][i]+";"+str(codigos2.get('p1_recop_dat'''+"-"+df2['p1_recop_dat'][i]))+";"+str(codigos2.get('p2_bases_dat'+"-"+df2['p2_bases_dat'][i]))+";"+str(codigos2.get('p3_control_base'+"-"+df2['p3_control_base'][i]))+";"+str(codigos2.get('p4_integr_dat'+"-"+df2['p4_integr_dat'][i]))+";"+str(codigos2.get('p5_tec_integr_dat'+"-"+df2['p5_tec_integr_dat'][i]))+";"+str(codigos2.get('p6_documen_pol_int'+"-"+df2['p6_documen_pol_int'][i]))+";"+str(codigos2.get('p7_area_cienc_dat'+"-"+df2['p7_area_cienc_dat'][i]))+";"+str(codigos2.get('p8_apli_tec_ciencia_dat'+"-"+df2['p8_apli_tec_ciencia_dat'][i]))+";"+str(codigos2.get('p9_herra_vis_dat'+"-"+df2['p9_herra_vis_dat'][i]))+";"+str(codigos2.get('p10_apli_pred'+"-"+df2['p10_apli_pred'][i]))+";"+str(codigos2.get('p11_documen_proy_cie_dat'+"-"+df2['p11_documen_proy_cie_dat'][i]))+";"+str(codigos2.get('p12_tien_report'+"-"+df2['p12_tien_report'][i]))+";"+str(codigos2.get('p13_act_report'+"-"+df2['p13_act_report'][i]))+";"+str(codigos2.get('p14_forma_act_report'+"-"+df2['p14_forma_act_report'][i]))+";"+str(codigos2.get('p15_disp_dat'+"-"+df2['p15_disp_dat'][i]))+";"+str(codigos2.get('p16_lect_dat_stand'+"-"+df2['p16_lect_dat_stand'][i]))+";"+str(codigos2.get('p17_api_dat'+"-"+df2['p17_api_dat'][i]))+";"+str(codigos2.get('p18_norm_prot_dat'+"-"+df2['p18_norm_prot_dat'][i]))+";"+str(codigos2.get('p19_tiemp_dat'+"-"+df2['p19_tiemp_dat'][i]))+";"+str(codigos2.get('p20_clasif_dat_sens'+"-"+df2['p20_clasif_dat_sens'][i]))+";"+str(codigos2.get('p21_dat_sens_consent'+"-"+df2['p21_dat_sens_consent'][i]))+";"+str(codigos2.get('p22_destr_minim_dat'+"-"+df2['p22_destr_minim_dat'][i]))+";"+str(codigos2.get('p23_espac_disc_prot_dat'+"-"+df2['p23_espac_disc_prot_dat'][i]))+";"+str(codigos2.get('p24_lineam_pro_dat'+"-"+df2['p24_lineam_pro_dat'][i]))+";"+str(codigos2.get('p25_roles_dat'+"-"+df2['p25_roles_dat'][i]))+";"+str(codigos2.get('p26_polit_uso_dat'+"-"+df2['p26_polit_uso_dat'][i]))+";"+str(codigos2.get('p27_documen_cic_v_dat'+"-"+df2['p27_documen_cic_v_dat'][i]))+";"+str(codigos2.get('p28_capac_pers_gobern_dat'+"-"+df2['p28_capac_pers_gobern_dat'][i]))+";"+str(codigos2.get('p29_report_acc_dat'+"-"+df2['p29_report_acc_dat'][i]))+";"+str(codigos2.get('p30_sist_acc_info'+"-"+df2['p30_sist_acc_info'][i]))+";"+str(codigos2.get('p31_revis_cal_dat'+"-"+df2['p31_revis_cal_dat'][i]))+";"+df2['p32_metr_cal_dat'][i]+";"+str(codigos2.get('p33_proc_cal_dat'+"-"+df2['p33_proc_cal_dat'][i]))+";"+str(codigos2.get('p34_per_espec_cal_dat'+"-"+df2['p34_per_espec_cal_dat'][i]))+";"+str(codigos2.get('p35_interc_dat'+"-"+df2['p35_interc_dat'][i]))+";"+str(codigos2.get('p36_mecan_prop_comp_dat'+"-"+df2['p36_mecan_prop_comp_dat'][i]))+";"+str(codigos2.get('p37_pers_espec_reut_dat'+"-"+df2['p37_pers_espec_reut_dat'][i]))+";"+str(codigos2.get('p38_model_dat'+"-"+df2['p38_model_dat'][i]))+";"+str(codigos2.get('p39_grad_documen_dat'+"-"+df2['p39_grad_documen_dat'][i]))+";"+df2['p40_documen_mod_dat'][i]+";"+str(codigos2.get('p41_pers_espec_model_dat'+"-"+df2['p41_pers_espec_model_dat'][i]))    
+    linea = df2['fecha'][i]+" ; "+df2['score'][i]+";"+df2['nombre_apellido'][i]+";"+df2['pertenece_gcba'][i]+";"+df2['mail'][i]+";"+df2['cargo'][i]+";"+df2['secre_subse_area'][i]+";"+str(codigos2.get('p1_recop_dat'''+"-"+df2['p1_recop_dat'][i]))+";"+str(codigos2.get('p2_bases_dat'+"-"+df2['p2_bases_dat'][i]))+";"+str(codigos2.get('p3_control_base'+"-"+df2['p3_control_base'][i]))+";"+str(codigos2.get('p4_integr_dat'+"-"+df2['p4_integr_dat'][i]))+";"+str(codigos2.get('p5_tec_integr_dat'+"-"+df2['p5_tec_integr_dat'][i]))+";"+str(codigos2.get('p6_documen_pol_int'+"-"+df2['p6_documen_pol_int'][i]))+";"+str(codigos2.get('p7_area_cienc_dat'+"-"+df2['p7_area_cienc_dat'][i]))+";"+str(codigos2.get('p8_apli_tec_ciencia_dat'+"-"+df2['p8_apli_tec_ciencia_dat'][i]))+";"+str(codigos2.get('p9_herra_vis_dat'+"-"+df2['p9_herra_vis_dat'][i]))+";"+str(codigos2.get('p10_apli_pred'+"-"+df2['p10_apli_pred'][i]))+";"+str(codigos2.get('p11_documen_proy_cie_dat'+"-"+df2['p11_documen_proy_cie_dat'][i]))+";"+str(codigos2.get('p12_tien_report'+"-"+df2['p12_tien_report'][i]))+";"+str(codigos2.get('p13_act_report'+"-"+df2['p13_act_report'][i]))+";"+str(codigos2.get('p14_forma_act_report'+"-"+df2['p14_forma_act_report'][i]))+";"+str(codigos2.get('p15_disp_dat'+"-"+df2['p15_disp_dat'][i]))+";"+str(codigos2.get('p16_lect_dat_stand'+"-"+df2['p16_lect_dat_stand'][i]))+";"+str(codigos2.get('p17_api_dat'+"-"+df2['p17_api_dat'][i]))+";"+str(codigos2.get('p18_norm_prot_dat'+"-"+df2['p18_norm_prot_dat'][i]))+";"+str(codigos2.get('p19_tiemp_dat'+"-"+df2['p19_tiemp_dat'][i]))+";"+str(codigos2.get('p20_clasif_dat_sens'+"-"+df2['p20_clasif_dat_sens'][i]))+";"+str(codigos2.get('p21_dat_sens_consent'+"-"+df2['p21_dat_sens_consent'][i]))+";"+str(codigos2.get('p22_destr_minim_dat'+"-"+df2['p22_destr_minim_dat'][i]))+";"+str(codigos2.get('p23_espac_disc_prot_dat'+"-"+df2['p23_espac_disc_prot_dat'][i]))+";"+str(codigos2.get('p24_lineam_pro_dat'+"-"+df2['p24_lineam_pro_dat'][i]))+";"+str(codigos2.get('p25_roles_dat'+"-"+df2['p25_roles_dat'][i]))+";"+str(codigos2.get('p26_polit_uso_dat'+"-"+df2['p26_polit_uso_dat'][i]))+";"+str(codigos2.get('p27_documen_cic_v_dat'+"-"+df2['p27_documen_cic_v_dat'][i]))+";"+str(codigos2.get('p28_capac_pers_gobern_dat'+"-"+df2['p28_capac_pers_gobern_dat'][i]))+";"+str(codigos2.get('p29_report_acc_dat'+"-"+df2['p29_report_acc_dat'][i]))+";"+str(codigos2.get('p30_sist_acc_info'+"-"+df2['p30_sist_acc_info'][i]))+";"+str(codigos2.get('p31_revis_cal_dat'+"-"+df2['p31_revis_cal_dat'][i]))+";"+df2['p32_metr_cal_dat'][i].replace(";", " - ")+";"+str(codigos2.get('p33_proc_cal_dat'+"-"+df2['p33_proc_cal_dat'][i]))+";"+str(codigos2.get('p34_per_espec_cal_dat'+"-"+df2['p34_per_espec_cal_dat'][i]))+";"+str(codigos2.get('p35_interc_dat'+"-"+df2['p35_interc_dat'][i]))+";"+str(codigos2.get('p36_mecan_prop_comp_dat'+"-"+df2['p36_mecan_prop_comp_dat'][i]))+";"+str(codigos2.get('p37_pers_espec_reut_dat'+"-"+df2['p37_pers_espec_reut_dat'][i]))+";"+str(codigos2.get('p38_model_dat'+"-"+df2['p38_model_dat'][i]))+";"+str(codigos2.get('p39_grad_documen_dat'+"-"+df2['p39_grad_documen_dat'][i]))+";"+df2['p40_documen_mod_dat'][i].replace(";", " - ")+";"+str(codigos2.get('p41_pers_espec_model_dat'+"-"+df2['p41_pers_espec_model_dat'][i]))    
     f.write(linea+"\n")
 f.close()    
-#sys.exit()
+
+
 
 df4 = pd.read_csv("nuevo_df.csv", names=colnames,index_col=False, encoding='utf-8',sep=';')
+
 del df4['sugerencias']
 
 print("*-*-*-*-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
@@ -234,6 +236,7 @@ for i in df4.index:
    
 f.close()    
 p.close()    
+
 
 
 """
@@ -489,23 +492,84 @@ for i in df_dim.index:
     if sum40 >= 100:
         sum40 = 0
    
-    total_dim1  = (sum_dim1  / (len(var_dim1) - noaplica_dim1))  *   len(var_dim1) 
-    total_dim2  = (sum_dim2  / (len(var_dim2) - noaplica_dim2))  *   len(var_dim2) 
-    total_dim3  = (sum_dim3  / (len(var_dim3) - noaplica_dim3))  *   len(var_dim3)  
-    total_dim4  = (sum_dim4  / (len(var_dim4) - noaplica_dim4))  *   len(var_dim4)  
-    total_dim5  = (sum_dim5  / (len(var_dim5) - noaplica_dim5))  *   len(var_dim5) 
-    
-    
-    jj = (len(var_dim6) - noaplica_dim6)
-    if jj == 0:
-        total_dim6 = 0
+    if (len(var_dim1) - noaplica_dim1) >0:
+        total_dim1  = (sum_dim1  / (len(var_dim1) - noaplica_dim1))  *   len(var_dim1)
     else:
-        total_dim6  = (sum_dim6  / (len(var_dim6) - noaplica_dim6))  *   len(var_dim6)  
+        total_dim1 = 0
+
+    if (len(var_dim2) - noaplica_dim2) >0:
+        total_dim2  = (sum_dim2  / (len(var_dim2) - noaplica_dim2))  *   len(var_dim2)
+    else:
+        total_dim2 = 0
+
+    if (len(var_dim3) - noaplica_dim3) >0:
+        total_dim3  = (sum_dim3  / (len(var_dim3) - noaplica_dim3))  *   len(var_dim3)
+    else:
+        total_dim3 = 0
+
+    if (len(var_dim4) - noaplica_dim4) >0:
+        total_dim4  = (sum_dim4  / (len(var_dim4) - noaplica_dim4))  *   len(var_dim4)
+    else:
+        total_dim4 = 0
+
+    if (len(var_dim5) - noaplica_dim5) >0:
+        total_dim5  = (sum_dim5  / (len(var_dim5) - noaplica_dim5))  *   len(var_dim5)
+    else:
+        total_dim5 = 0
+
+    if (len(var_dim6) - noaplica_dim6) >0:
+        total_dim6  = (sum_dim6  / (len(var_dim6) - noaplica_dim6))  *   len(var_dim6)
+    else:
+        total_dim6 = 0
+
+    if (len(var_dim7) - noaplica_dim7) >0:
+        total_dim7  = (sum_dim7  / (len(var_dim7) - noaplica_dim7))  *   len(var_dim7)
+    else:
+        total_dim7 = 0
+
+    if (len(var_dim8) - noaplica_dim8) >0:
+        total_dim8  = (sum_dim8  / (len(var_dim8) - noaplica_dim8))  *   len(var_dim8)
+    else:
+        total_dim8 = 0
+
+    if (len(var_dim9) - noaplica_dim9) >0:
+        total_dim9  = (sum_dim9  / (len(var_dim9) - noaplica_dim9))  *   len(var_dim9)
+    else:
+        total_dim9 = 0
+
+    if (len(var_dim10) - noaplica_dim10) >0:
+        total_dim10  = (sum_dim10  / (len(var_dim10) - noaplica_dim10))  *   len(var_dim10)
+    else:
+        total_dim10 = 0
+
+
+
+
+
+
+
+    # total_dim2  = (sum_dim2  / (len(var_dim2) - noaplica_dim2))  *   len(var_dim2) 
+    # total_dim3  = (sum_dim3  / (len(var_dim3) - noaplica_dim3))  *   len(var_dim3)  
+    # total_dim4  = (sum_dim4  / (len(var_dim4) - noaplica_dim4))  *   len(var_dim4)  
+    # total_dim5  = (sum_dim5  / (len(var_dim5) - noaplica_dim5))  *   len(var_dim5) 
+    
+    
+    # # jj = (len(var_dim6) - noaplica_dim6)
+    # # if jj == 0:
+    # #     total_dim6 = 0
+    # # else:
         
-    total_dim7  = (sum_dim7  / (len(var_dim7) - noaplica_dim7))  *   len(var_dim7)  
-    total_dim8  = (sum_dim8  / (len(var_dim8) - noaplica_dim8))  *   len(var_dim8)  
-    total_dim9  = (sum_dim9  / (len(var_dim9) - noaplica_dim9))  *   len(var_dim9)  
-    total_dim10 = (sum_dim10 / (len(var_dim10)- noaplica_dim10)) *   len(var_dim10)
+    # print("sum_dim6",sum_dim6)
+    # print("len(var_dim6)",len(var_dim6))
+    # print("noaplica_dim6",noaplica_dim6)
+    # print("(len(var_dim6) - noaplica_dim6)",(len(var_dim6) - noaplica_dim6))
+    
+    # total_dim6  = (sum_dim6  / (len(var_dim6) - noaplica_dim6))  *   len(var_dim6)  
+        
+    # total_dim7  = (sum_dim7  / (len(var_dim7) - noaplica_dim7))  *   len(var_dim7)  
+    # total_dim8  = (sum_dim8  / (len(var_dim8) - noaplica_dim8))  *   len(var_dim8)  
+    # total_dim9  = (sum_dim9  / (len(var_dim9) - noaplica_dim9))  *   len(var_dim9)  
+    # total_dim10 = (sum_dim10 / (len(var_dim10)- noaplica_dim10)) *   len(var_dim10)
    
     print("dimension 1:"+str(total_dim1))
     print("dimension 2:"+str(total_dim2))
